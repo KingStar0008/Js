15a47d0bfa09b63587afc1b6e1d9018c324f1303
KingStar0008 <shivank.mishra1@tothenew.com>
2025-02-25 19:17:10 +0000

    Day1

---
+++ JavaScript/01_Variable.js
@@ -0,0 +1,15 @@
+const accountId = 23545345
+let accountEmail = "mishrashivank2001@gmail.com"
+var accountPassword = "1232432"
+accountCity = "Jaipur"
+let accountState
+
+// accountId = 45352554
+// console.log(accountId)
+// accountEmail = "shivankmishra0912@gmail.com"
+// console.log(accountEmail)
+// accountPassword = "34324545"
+// console.log(accountPassword)
+// accountCity = "Delhi"
+// console.log(accountCity)
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
