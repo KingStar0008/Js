6f0e5af0713b400ec794b3ebf017f1eadcaed08a
KingStar0008 <shivank.mishra1@tothenew.com>
2025-03-24 11:41:15 +0000

    Operations

---
+++ JavaScript/03_ConversionOperation.js
@@ -0,0 +1,39 @@
+// let score = "33"
+// console.log(typeof score)
+
+// let value1 = score
+// console.log(typeof value1)
+
+// let value = Number(score)
+// console.log(typeof value)
+
+// "33" => 33
+// "33abc" => NaN [Not a Number]
+// true => 1
+// Null => 0
+//undefined => NaN 
+
+
+// let booleanCheck = "abc0";
+// let boolCheck = Boolean(booleanCheck)
+// console.log(boolCheck);
+
+// In Boolean Conversion Output:
+// 1 => true [if number then true]
+// "" or null or undefined => false [if empty false]
+// "hitesh" => true [if string then true]
+
+
+
+
+// *********************** Operations **********************
+
+
+let value = 4
+let negValue = -value  // will print negative value of 4
+console.log(negValue);
+
+console.log("1" + 2)
+console.log(1 + "2")
+console.log("1" + 2 + 2)
+console.log(1 + 2 + "2")
