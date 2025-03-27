69c908e96bc514783ae6ad4a23f76899cc130050
KingStar0008 <shivank.mishra1@tothenew.com>
2025-03-27 07:06:33 +0000

    Update

--- JavaScript/02_DataType.js
+++ JavaScript/02_DataType.js
@@ -14,6 +14,46 @@
 
 
 // Object
-console.log(typeof "Shivank") // returns the type of datatype.
-console.log(typeof null) // Output => [Object]
-console.log(typeof undefined) //Output => [undefined]
+// console.log(typeof "Shivank") // returns the type of datatype.
+// console.log(typeof null) // Output => [Object]
+// console.log(typeof undefined) //Output => [undefined]
+
+
+// #Primitive // Values goes to Stack Memory
+// 7 Types : String, Number, Boolean, null, undefined, Symbol,
+//           BigInt
+
+// #Reference Type (Non Primitive) // Value goes to Heap Memory
+// Array, Objects, Functions 
+
+
+
+
+
+
+
+// ********************************* Memory Sumarry *************************
+
+// When you take reference from Heap then Original value is returned not the referenced value.
+ 
+
+let userOne = {
+    name: "Shivank"
+}
+
+let userTwo = userOne
+
+console.log(userTwo)
+console.log(userOne == userTwo)
+
+
+// Backtick's is used to get multiline feature, proper format and handling dynamic value.
+// In backtics we use ${} to provide direct value and perform action on these values directly.
+
+console.log("My name is: " + "Shivank" + 12)
+const poem = `Roses are red
+Violets are blue,
+JavaScript is fun,
+And so are you!`;
+
+console.log(poem);
